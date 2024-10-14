e266458da65cb6730c5cc1f2d39b1060e6ac9f8c
Yosr <yosrabbassi0@gmail.com>
2024-10-14 09:33:03 +0200

    first commit

---
+++ Frontend/angular-tailwind-main/src/app/shared/event-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../services/event.service';
+
+@Component({
+  selector: 'app-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.css']
+})
+export class EventListComponent implements OnInit {
+  events: any[] = [];
+
+  constructor(private eventService: EventService) {}
+
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data;
+    });
+  }
+
+  register(eventId: string): void {
+    this.eventService.registerToEvent(eventId).subscribe(() => {
+      console.log('Inscription réussie');
+    });
+  }
+}
